fa5865745554df1247bf373cb6d224c13edc8acc
Hieu <hieu@Hieuts-Mac-Pro.local>
2016-05-26 14:36:32 +0700

    MunchPak

---
+++ php/01_MunchPak_Checkout_v2.php
@@ -0,0 +1,28 @@
+<?php  include('head.php')?>
+
+<body class='homepage' id='homepage'>
+
+     <div class='main' id='xxx' custom='awwwwwwww'>Weldone
+
+     </div>
+
+
+     <link rel='stylesheet/less' type='text/css' href='css/hieut.style.less' />
+
+
+     <script src='js/owl.carousel.js' type='text/javascript'>
+     </script>
+
+
+     <script src='js/sli.common.js' type='text/javascript'>
+     </script>
+
+
+     <script src='js/main.menu.js' type='text/javascript'>
+     </script>
+
+
+     <script src='js/less.min.js' type='text/javascript'>
+     </script>
+<?php  include('footer.php')?>
+</body>
